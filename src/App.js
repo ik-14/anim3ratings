cea5ee284c2ffe0b687d844de9ba3feb5ba4163c
Ismail Khalil <ismailkhalil@hollandandbarrett.com>
2023-02-05 16:17:54 +0000

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
